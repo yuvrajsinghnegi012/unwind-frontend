a6b86b9abc2c7cb89f68b3c64040433432907845
yuvrajsinghnegi012 <144053547+yuvrajsinghnegi012@users.noreply.github.com>
2024-06-02 16:54:44 +0530

    Redux-toolkit setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,21 @@ import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import ReactDOM from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
+import { Provider } from "react-redux";
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
+import { store } from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <DndProvider backend={HTML5Backend}>
-        <App />
-        <Toaster />
-      </DndProvider>
+      <Provider store={store}>
+        <DndProvider backend={HTML5Backend}>
+          <App />
+          <Toaster />
+        </DndProvider>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 )
