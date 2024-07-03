c3f9541bee85c7ad2644447440e0268cf5b4098e
yuvrajsinghnegi012 <144053547+yuvrajsinghnegi012@users.noreply.github.com>
2024-07-03 16:27:31 +0530

    updated the name and constant images

--- src/constant.js
+++ src/constant.js
@@ -80,6 +80,22 @@ import bannerImg3 from "../assets/homepage/3.jpg";
 import bannerImg4 from "../assets/homepage/4.jpg";
 import bannerImg5 from "../assets/homepage/5.jpg";
 
+import beach_cat from "../assets/beach_cat.jpg";
+import windmill_cat from "../assets/windmill_cat.webp";
+import modern_cat from "../assets/modern_cat.webp"
+import countryside_cat from "../assets/countryside_cat.webp";
+import pool_cat from "../assets/pool_cat.jpg";
+import island_cat from "../assets/island_cat.webp";
+import lake_cat from "../assets/lake_cat.webp";
+import skiing_cat from "../assets/skiing_cat.jpg";
+import castle_cat from "../assets/castle_cat.webp";
+import cave_cat from "../assets/cave_cat.jpg";
+import camping_cat from "../assets/camping_cat.jpg";
+import arctic_cat from "../assets/arctic_cat.webp";
+import desert_cat from "../assets/desert_cat.webp";
+import barn_cat from "../assets/barn_cat.jpg";
+import lux_cat from "../assets/lux_cat.jpg";;
+
 export const bannerImages = [bannerImg1, bannerImg2, bannerImg3, bannerImg4, bannerImg5];
 
 export const categories = [
@@ -88,91 +104,91 @@ export const categories = [
     icon: BiWorld,
   },
   {
-    img: "../assets/beach_cat.jpg",
+    img: beach_cat,
     label: "Beachfront",
     icon: TbBeach,
     description: "This property is close to the beach!",
   },
   {
-    img: "../assets/windmill_cat.webp",
+    img: windmill_cat,
     label: "Windmills",
     icon: GiWindmill,
     description: "This property is has windmills!",
   },
   {
-    img: "../assets/modern_cat.webp",
+    img: modern_cat,
     label: "Iconic cities",
     icon: MdOutlineVilla,
     description: "This property is modern!",
   },
   {
-    img: "../assets/countryside_cat.webp",
+    img: countryside_cat,
     label: "Countryside",
     icon: TbMountain,
     description: "This property is in the countryside!",
   },
   {
-    img: "../assets/pool_cat.jpg",
+    img: pool_cat,
     label: "Amazing Pools",
     icon: TbPool,
     description: "This is property has a beautiful pool!",
   },
   {
-    img: "../assets/island_cat.webp",
+    img: island_cat,
     label: "Islands",
     icon: GiIsland,
     description: "This property is on an island!",
   },
   {
-    img: "../assets/lake_cat.webp",
+    img: lake_cat,
     label: "Lakefront",
     icon: GiBoatFishing,
     description: "This property is near a lake!",
   },
   {
-    img: "../assets/skiing_cat.jpg",
+    img: skiing_cat,
     label: "Ski-in-out",
     icon: FaSkiing,
     description: "This property has skiing activies!",
   },
   {
-    img: "../assets/castle_cat.webp",
+    img: castle_cat,
     label: "Castles",
     icon: GiCastle,
     description: "This property is an ancient castle!",
   },
   {
-    img: "../assets/cave_cat.jpg",
+    img: cave_cat,
     label: "Caves",
     icon: GiCaveEntrance,
     description: "This property is in a spooky cave!",
   },
   {
-    img: "../assets/camping_cat.jpg",
+    img: camping_cat,
     label: "Camping",
     icon: GiForestCamp,
     description: "This property offers camping activities!",
   },
   {
-    img: "../assets/arctic_cat.webp",
+    img: arctic_cat,
     label: "Arctic",
     icon: BsSnow,
     description: "This property is in arctic environment!",
   },
   {
-    img: "../assets/desert_cat.webp",
+    img: desert_cat,
     label: "Desert",
     icon: GiCactus,
     description: "This property is in the desert!",
   },
   {
-    img: "../assets/barn_cat.jpg",
+    img: barn_cat,
     label: "Barns",
     icon: GiBarn,
     description: "This property is in a barn!",
   },
   {
-    img: "../assets/lux_cat.jpg",
+    img: lux_cat,
     label: "Luxury",
     icon: IoDiamond,
     description: "This property is brand new and luxurious!",
